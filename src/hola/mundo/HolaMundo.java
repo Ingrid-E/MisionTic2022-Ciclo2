efffb12990a5d22a7ea4b306f8be5d6d504378a6
Ingrid E <ingrid.echeverri@correounivalle.edu.co>
2021-07-19 11:49:41 -0500

    Testing

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,28 @@
+/*
+ * @author Ingrid Echeverri (https://github.com/Ingrid-E)
+ * @version
+ * @file hola.mundo
+ * @date    
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hola.mundo;
+
+/**
+ * Main class
+ * @returns
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
