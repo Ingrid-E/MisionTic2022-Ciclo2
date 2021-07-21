985cb8bc4569c66f4c13c6a8273a7d08622ec637
Ingrid E <ingrid.echeverri@correounivalle.edu.co>
2021-07-21 14:14:16 -0500

    Clase dia 2

--- src/clase1/Clase1.java
+++ src/clase1/Clase1.java
@@ -18,6 +18,7 @@ public class Clase1 {
     public static void main(String[] args) {
         // TODO code application logic here
        System.out.println("Hola Mundo");
+       Clase2 dia2 = new Clase2();
     }
 
 }
