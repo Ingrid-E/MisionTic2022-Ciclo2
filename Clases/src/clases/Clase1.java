0fa52a2c3f8713cddb1d2cf503fd25aaf86ed6f7
Ingrid E <ingrid.echeverri@correounivalle.edu.co>
2021-07-27 10:09:26 -0500

    Test

---
+++ Clases/src/clases/Clase1.java
@@ -0,0 +1,26 @@
+/*
+ * @author Ingrid Echeverri (https://github.com/Ingrid-E)
+ * @version 0.1
+ * @file clase1.java
+ * @date 19/07/2021
+ */
+
+package clases;
+
+/**
+ * Main class to test "Hello world"
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Hola Mundo");
+       //Clase2 dia2 = new Clase2();
+       //Clase3 dia3 = new Clase3();
+       Clases4 dia4 = new Clases4();
+    }
+
+}
